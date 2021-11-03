a885c8a055e3119846862a44eb1ec9dd543af6e0
ang2tea <zazasu12@gmail.com>
2021-11-04 02:40:23 +0300

    Implementing the Quick Sort Function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
 }
 
 fn QuickSort(mass: Vec<i32>) -> Vec<i32>{
-    if (mass.len() <= 1){
+    let randNum: i32;
+    if (mass.len() <= 1) {
         return mass;
+    } else {
+        randNum = rand::thread_rng().gen_range(1..mass.len());
+        println!("{:?}", randNum);
     }
 
+    let mut l_nums  = Vec::from(mass[randNum..mass.len()]);
+    let mut e_nums = vec![randNum; mass.count(randNum)];
+    let mut b_nums= Vec::from(mass[0..randNum]);
 
-    return mass;
+    let mut returElemAdd: Vec<i32>;
+    returElemAdd.append(&mut l_nums);
+    returElemAdd.append(&mut e_nums);
+    returElemAdd.append(&mut b_nums);
+
+    let returElem= returElemAdd.clone();
+    return returElem;
 }
