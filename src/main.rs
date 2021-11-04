490ef078339f6dadc9125c07ebeb6144898bbd12
ang2tea <zazasu12@gmail.com>
2021-11-04 03:12:04 +0300

    End of test and error correction

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 use rand::Rng;
 
 fn main() {
+    let test = vec!(3, 1, 2, 0, 4, 6, 8, 5, 7);
+    println!("{:?}", QuickSort(test));
 }
 
 fn QuickSort(mass: Vec<i32>) -> Vec<i32>{
-    let randNum: i32;
+    let randNum: usize;
     if (mass.len() <= 1) {
         return mass;
     } else {
         randNum = rand::thread_rng().gen_range(1..mass.len());
-        println!("{:?}", randNum);
     }
 
-    let mut l_nums  = Vec::from(mass[randNum..mass.len()]);
-    let mut e_nums = vec![randNum; mass.count(randNum)];
-    let mut b_nums= Vec::from(mass[0..randNum]);
+    let mut l_nums= Vec::new();
+    let mut e_nums= vec!(mass[randNum]);
+    let mut b_nums= Vec::new();
+    for i in mass.iter(){
+        if (i < &mass[randNum]){
+            l_nums.push(i.clone());
+        }
+        else if (i > &mass[randNum]){
+            b_nums.push(i.clone());
+        }
+    }
 
-    let mut returElemAdd: Vec<i32>;
-    returElemAdd.append(&mut l_nums);
+    let mut returElemAdd: Vec<i32> = Vec::new();
+    returElemAdd.append(&mut QuickSort(l_nums));
     returElemAdd.append(&mut e_nums);
-    returElemAdd.append(&mut b_nums);
-
+    returElemAdd.append(&mut QuickSort(b_nums));
     let returElem= returElemAdd.clone();
     return returElem;
 }
